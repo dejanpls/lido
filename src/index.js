dcd74a08dedd1eaeff9dfaf020ea9fe0401de726
dejanpls <68787353+dejanpls@users.noreply.github.com>
2025-02-06 18:21:16 +0100

    feat: Implement 'tab switching' between content by pressing buttons

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-import "./styles.css";
+import { displayHome, displayReservation, displayMenu } from "./content";
+
+displayHome();
+
+const reservationBtn = document.getElementById("btn-reserve");
+const homeBtn = document.getElementById("btn-home");
+const menuBtn = document.getElementById("btn-menu");
+
+reservationBtn.addEventListener('click', () => displayReservation());
+homeBtn.addEventListener('click', () => displayHome());
+menuBtn.addEventListener('click', () => displayMenu());
